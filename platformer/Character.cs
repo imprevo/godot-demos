c28902fb64bd40d971256fafed60af1e190356f0
imprevo <zhenek42@gmail.com>
2021-06-16 18:21:52 +0700

    add jump action

--- platformer/Character.cs
+++ platformer/Character.cs
@@ -3,14 +3,19 @@ using System;
 
 public class Character : KinematicBody2D
 {
-    private int _speed = 400;
-    private int _gravity = 300;
+    private int _acceleration = 900;
+    private int _maxSpeed = 400;
+    private int _friction = 900;
+    private int _gravity = 1000;
+    private int _jumpForce = -500;
+    private Vector2 _movement;
 
     private AnimatedSprite _animatedSprite;
     private AnimationNodeStateMachinePlayback _stateMachine;
 
     public override void _Ready()
     {
+        _movement = new Vector2();
         _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
         var animTree = GetNode<AnimationTree>("AnimationTree");
         animTree.Active = true;
@@ -20,42 +25,59 @@ public class Character : KinematicBody2D
     public override void _Process(float delta)
     {
         // var currentState = _stateMachine.GetCurrentNode();
-        var velocity = new Vector2();
 
-        if (Input.IsActionPressed("ui_right"))
+        if (Input.IsActionJustPressed("ui_up") && IsOnFloor())
         {
-            _animatedSprite.FlipH = false;
-            velocity.x = _speed;
-        }
-        else if (Input.IsActionPressed("ui_left"))
-        {
-            _animatedSprite.FlipH = true;
-            velocity.x = -_speed;
-        }
-        else if (Input.IsActionPressed("ui_up"))
-        {
-            velocity.y = -_gravity * 2;
-        }
-
-        RunAnimation(velocity);
-
-        velocity.y += _gravity;
-        MoveAndSlide(velocity);
-    }
-
-    private void RunAnimation(Vector2 velocity)
-    {
-        if (Input.IsActionPressed("ui_select"))
-        {
-            _stateMachine.Travel("attack1");
-        }
-        else if (velocity.Length() == 0)
-        {
-            _stateMachine.Travel("idle");
+            _movement.y = _jumpForce;
         }
         else
         {
+            _movement.y += _gravity * delta;
+        }
+
+        var movementY = _movement.y;
+        var inputX = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+
+        if (inputX == 0)
+        {
+            _movement = _movement.MoveToward(Vector2.Zero, _friction * delta);
+        }
+        else
+        {
+            _movement = _movement.MoveToward(new Vector2(inputX, 0) * _maxSpeed, _acceleration * delta);
+        }
+
+        _movement.y = movementY;
+
+        _movement = MoveAndSlide(_movement, Vector2.Up);
+        RunAnimation();
+
+    }
+
+    private void RunAnimation()
+    {
+        GD.Print(_movement);
+
+        if (Input.IsActionJustPressed("attack"))
+        {
+            _stateMachine.Travel("attack1");
+        }
+        else if (!IsOnFloor())
+        {
+            _stateMachine.Travel("jump");
+        }
+        else if (Math.Abs(_movement.x) > 0.001f)
+        {
             _stateMachine.Travel("run");
+        }
+        else
+        {
+            _stateMachine.Travel("idle");
+        }
+
+        if (Math.Abs(_movement.x) > 0.001f)
+        {
+            _animatedSprite.FlipH = _movement.x < 0;
         }
     }
 
