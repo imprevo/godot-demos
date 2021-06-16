232638db068b9e2fbbcf1bb598eea47f51a7e145
imprevo <zhenek42@gmail.com>
2021-06-16 15:58:59 +0700

    add animation state for character

--- platformer/Character.cs
+++ platformer/Character.cs
@@ -7,42 +7,66 @@ public class Character : KinematicBody2D
     private int _gravity = 300;
 
     private AnimatedSprite _animatedSprite;
+    private AnimationNodeStateMachinePlayback _stateMachine;
 
     public override void _Ready()
     {
         _animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        var animTree = GetNode<AnimationTree>("AnimationTree");
+        animTree.Active = true;
+        _stateMachine = (AnimationNodeStateMachinePlayback)animTree.Get("parameters/playback");
     }
 
     public override void _Process(float delta)
     {
+        // var currentState = _stateMachine.GetCurrentNode();
         var velocity = new Vector2();
-        velocity.y = _gravity;
 
         if (Input.IsActionPressed("ui_right"))
         {
             _animatedSprite.FlipH = false;
-            _animatedSprite.Play("run");
             velocity.x = _speed;
         }
         else if (Input.IsActionPressed("ui_left"))
         {
             _animatedSprite.FlipH = true;
-            _animatedSprite.Play("run");
             velocity.x = -_speed;
         }
         else if (Input.IsActionPressed("ui_up"))
         {
             velocity.y = -_gravity * 2;
         }
-        else if (Input.IsActionPressed("ui_select"))
+
+        RunAnimation(velocity);
+
+        velocity.y += _gravity;
+        MoveAndSlide(velocity);
+    }
+
+    private void RunAnimation(Vector2 velocity)
+    {
+        if (Input.IsActionPressed("ui_select"))
         {
-            _animatedSprite.Play("attack");
+            _stateMachine.Travel("attack1");
+        }
+        else if (velocity.Length() == 0)
+        {
+            _stateMachine.Travel("idle");
         }
         else
         {
-            _animatedSprite.Play("idle");
+            _stateMachine.Travel("run");
         }
+    }
 
-        MoveAndSlide(velocity);
+    public void Hurt()
+    {
+        _stateMachine.Travel("hurt");
+    }
+
+    public void Die()
+    {
+        _stateMachine.Travel("death");
+        SetPhysicsProcess(false);
     }
 }
