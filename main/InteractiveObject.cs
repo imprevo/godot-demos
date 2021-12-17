c45185240b82e9054db67152db8d8755ebceb1e8
imprevo <zhenek42@gmail.com>
2021-12-17 20:26:59 +0700

    add confirmation dialog before changing a scene

--- main/InteractiveObject.cs
+++ main/InteractiveObject.cs
@@ -6,10 +6,39 @@ namespace Main
     public class InteractiveObject : CollisionShape2D
     {
         [Export]
+        private NodePath path;
+
+        [Export]
         public Scene sceneKey = Scene.UNKNOWN;
+
+        private ConfirmationDialog _dialog;
+
+        public override void _Ready()
+        {
+            _dialog = GetNode<ConfirmationDialog>(path);
+        }
 
         public void Interact()
         {
+            _dialog.Popup_();
+            SubscribeOnDialogSignals();
+        }
+
+        private void SubscribeOnDialogSignals()
+        {
+            _dialog.Connect("confirmed", this, nameof(ActionConfirmed));
+            _dialog.Connect("popup_hide", this, nameof(UnsubscribeFromDialogSignals));
+        }
+
+        private void UnsubscribeFromDialogSignals()
+        {
+            _dialog.Disconnect("confirmed", this, nameof(ActionConfirmed));
+            _dialog.Disconnect("popup_hide", this, nameof(UnsubscribeFromDialogSignals));
+        }
+
+        private void ActionConfirmed()
+        {
+            _dialog.Hide();
             SceneManager.manager.ChangeScene(sceneKey);
         }
     }
